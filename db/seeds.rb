e995ecad6d203231af8749a9f401bf9847b0c6d6
OnanThairu <thairuonan@gmail.com>
2022-09-05 12:43:43 +0300

    Create seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,34 @@
-puts "🌱 Seeding spices..."
+puts "Seeding users..."
 
-# Seed your database here
+User.create([
+  { username: "Onan" },
+  { username: "Ken"  },
+  { username: "Mike" },
+  { username: "Rachel"},
+  { username: "Joy"  }
+])
+
+puts "Seeding blockers..."
+
+Blocker.create([
+  {
+    username: "Onan",
+    description: "How to loop though an array in ruby",
+    tag: "Ruby",
+    solution: "Use .map, .each, .select, .sum"
+  },
+  {
+    username: "Mike",
+    description: "How to loop though an array in ruby",
+    tag: "JavaScript",
+    solution: "Use .map, .reduce, .filter"
+  },
+  {
+    username: "Rachel",
+    description: "How to create a new react application.",
+    tag: "React",
+    solution: "Use npx create-react-app <appname>"
+  }
+])
 
 puts "✅ Done seeding!"
