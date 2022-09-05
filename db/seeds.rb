cd3c039acf96fd2daa8856bcb90a7ed48f345c55
OnanThairu <thairuonan@gmail.com>
2022-09-05 14:36:56 +0300

    Update seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
 puts "Seeding users..."
 
-User.create([
-  { username: "Onan" },
-  { username: "Ken"  },
-  { username: "Mike" },
-  { username: "Rachel"},
-  { username: "Joy"  }
-])
+onan = User.create({ username: "Onan" })
+ken = User.create({ username: "Ken" })
+mike = User.create({ username: "Mike" })
+rachel = User.create({ username: "Rachel" })
+joy = User.create({ username: "Joy" })
 
 puts "Seeding blockers..."
 
@@ -15,19 +13,50 @@ Blocker.create([
     username: "Onan",
     description: "How to loop though an array in ruby",
     tag: "Ruby",
-    solution: "Use .map, .each, .select, .sum"
+    solution: "Use .map, .each, .select, .sum",
+    user_id: "#{onan.id}"
   },
   {
     username: "Mike",
     description: "How to loop though an array in ruby",
     tag: "JavaScript",
-    solution: "Use .map, .reduce, .filter"
+    solution: "Use .map, .reduce, .filter",
+    user_id: "#{mike.id}"
   },
   {
     username: "Rachel",
     description: "How to create a new react application.",
     tag: "React",
-    solution: "Use npx create-react-app <appname>"
+    solution: "Use npx create-react-app <appname>",
+    user_id: "#{rachel.id}"
+  },
+  {
+    username: "Onan",
+    description: "How to create to run active record migrations",
+    tag: "Ruby",
+    solution: "Use rake task db:migrate",
+    user_id: "#{onan.id}"
+  },
+  {
+    username: "Rachel",
+    description: "How to create a new react application.",
+    tag: "React",
+    solution: "Use npx create-react-app <appname>",
+    user_id: "#{rachel.id}"
+  },
+  {
+    username: "Rachel",
+    description: "How to create a new react application.",
+    tag: "React",
+    solution: "Use npx create-react-app <appname>",
+    user_id: "#{onan.id}"
+  },
+  {
+    username: "Rachel",
+    description: "How to create a new react application.",
+    tag: "React",
+    solution: "Use npx create-react-app <appname>",
+    user_id: "#{mike.id}"
   }
 ])
 
